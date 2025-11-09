3472bacdfae313c0c6c6ea6705ecf2884dac6fa7
June Leoman Lapera <leomanlapera@gmail.com>
2025-11-09 21:44:38 +0800

    player animation

--- player/player.gd
+++ player/player.gd
@@ -2,9 +2,19 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+var input_vector: = Vector2.ZERO
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 func _physics_process(_delta: float) -> void:
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if input_vector != Vector2.ZERO:
+		update_blend_positions()
 
 	velocity = input_vector * SPEED
-
 	move_and_slide()
+
+func update_blend_positions() -> void:
+	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", input_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", input_vector)
